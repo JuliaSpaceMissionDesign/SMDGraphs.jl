9162698d2ae7a4fb2223c2c584ada77572c82475
MicheleCeresoli <michele.ceresoli7@gmai.com>
2023-05-01 22:29:23 +0200

    Imported bug fix from dev

--- src/SMDGraphs.jl
+++ src/SMDGraphs.jl
@@ -1,6 +1,17 @@
 module SMDGraphs
 
-# Write your package code here.
+import Graphs: 
+    SimpleGraph, 
+    SimpleDiGraph,
+
+    add_edge!,
+    add_vertex!, 
+    nv, 
+    dijkstra_shortest_paths, 
+    enumerate_paths, 
+    has_vertex, 
+    has_path   
+    
 include("abstract.jl")
 include("graphs/MappedGraphs.jl")
 
